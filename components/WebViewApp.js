130b42b9b2b87ea03c7759def2b690d119c28006
Facund <facund.fortuny@inbestme.com>
2023-08-01 18:26:35 +0200

    Add webview properties

--- components/WebViewApp.js
+++ components/WebViewApp.js
@@ -2,5 +2,12 @@ import { WebView } from "react-native-webview";
 
 export const WebViewApp = () => {
   const uri = `https://vue-onfido.vercel.app`;
-  return <WebView source={{ uri: uri }} style={{ flex: 1 }} />;
+  return (
+    <WebView
+      allowsInlineMediaPlayback={true}
+      mediaPlaybackRequiresUserAction={false}
+      source={{ uri: uri }}
+      style={{ flex: 1 }}
+    />
+  );
 };
